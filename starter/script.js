696e92c758f00367edc0ae8dfb1b32eafb8c7ffd
Muhammadaziz <133212735+Alanm17@users.noreply.github.com>
2024-05-23 23:43:51 +0900

    destructing objects with renaming technique

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,31 @@
 'use strict';
+// const arr = [2, 3, 4]; // destructuring does not effect to the original array [const]
+// let [x, v, z] = arr; /// destructuring the array and we have to declare variables using ""const""
+// console.log(x, v, z);
+// x, v, (z = [1, 2, 3]);
+// console.log(x, v, z);
 
+// let [firstS, , , secondS] = restaurant.categories;
+// console.log(firstS, secondS); // in this this comma ,, ⬆️ changes the order of values to be taken
+// //switching variables
+// // const temp = firstS;
+// // firstS = secondS;
+// // secondS = temp;
+
+// let [firstSs, secondSs] = [secondS, firstS];
+// console.log(firstS, secondS);
+
+// const [choco, shashlik] = restaurant.valueStruct(1, 2); // taking the value inside variables using method inside object and destructing them to different variables
+// console.log(choco, shashlik); // choco =pasta , shashlik = garlic bread
+
+// // nested destructuring
+// const nestedArray = [1, 2, [1, 3]];
+// const [i, , [o, b]] = nestedArray;
+// console.log(i, o, b);
+
+// // default values
+// const [d, l, m = 1, f] = [5, 6, 3, 3];
+// console.log(d, l, m, f);
 // Data needed for a later exercise
 
 // Data needed for first part of the section
@@ -9,35 +35,28 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  valueStruct(menuIndex, starterIndex) {
-    return [this.mainMenu[menuIndex], this.starterMenu[starterIndex]];
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+    arrayStruct(menuIndex, starterIndex) {
+      return [this.mainMenu[menuIndex], this.starterMenu[starterIndex]];
+    },
   },
 };
+// destructing objects . in this method ,we are just destructing restaurant properties with the same variables outside of the object literal .
+const { name, categories, openingHours } = restaurant;
+console.log(name, categories, openingHours); // Classico .. italian..,thu...
 
-const arr = [2, 3, 4]; // destructuring does not effect to the original array
-let [x, v, z] = arr; /// destructuring the array and we have to declare variables using ""const""
-console.log(x, v, z);
-x, v, (z = [1, 2, 3]);
-console.log(x, v, z);
-
-let [firstS, , , secondS] = restaurant.categories;
-console.log(firstS, secondS); // in this this comma ,, ⬆️ changes the order of values to be taken
-//switching variables
-// const temp = firstS;
-// firstS = secondS;
-// secondS = temp;
-
-let [firstSs, secondSs] = [secondS, firstS];
-console.log(firstS, secondS);
-
-const [choco, shashlik] = restaurant.valueStruct(1, 2); // taking the value inside variables using method inside object and destructing them to different variables
-console.log(choco, shashlik); // choco =pasta , shashlik = garlic bread
-
-// nested destructuring
-const nestedArray = [1, 2, [1, 3]];
-const [i, , [o, b]] = nestedArray;
-console.log(i, o, b);
-
-// default values
-const [d, l, m = 1, f] = [5, 6, 3, 3];
-console.log(d, l, m, f);
+// direct destructing with renaming technique
+const { name: nameOf, categories: tags, openingHours: hours } = restaurant;
+console.log(nameOf, tags, hours); // Classico .. italian..,thu...
